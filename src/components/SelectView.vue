2a7ba4c116825381b797abd102640ca6c006454c
MIN-GOL <imingol@qq.com>
2024-05-16 22:01:19 +0800

    feat - 搜索模块以及历史记录模块完成

--- src/components/SelectView.vue
+++ src/components/SelectView.vue
@@ -10,54 +10,130 @@ const key = 'U2FsdGvkx19WSQ59Cg+Fj9jNZPxRC5y0xB1iV06BeNA='
 
 const onCancel = () => value.value = '';
 const onClickLeft = () => history.back();
+const historyList = ref(JSON.parse(localStorage.getItem('historySearch')))
+
+console.log(historyList.value)
+// 搜索历史初始化
+if (historyList.value == null) {
+  localStorage.setItem('historySearch', JSON.stringify([]))
+}
 
 // 搜索请求
 const onSearch = (val) => {
-  axios.get(`${base_url}/search`,{
+  axios.get(`${base_url}/search`, {
     params: {
       name: val,
       appkey: key
     }
   }).then(function (res) {
-    console.log(res.data.result)
-    resp.value = res.data.result}
-  )
+    let resp = res.data.result
+
+    // 搜索成功
+    if (resp.length > 0) {
+      showToast('搜索成功')
+      let flag = 0
+      let list = [{title: val}]
+      // 判断是否存在相同的搜索记录
+      for (const item of historyList.value) {
+        // 存在 -> 不作操作
+        if (item['title'] === val) {
+          flag = 1
+        }
+      }
+      // 不存在 -> 存入本地localStorage
+      if (flag === 0) {
+        historyList.value.push({title: val})
+        localStorage.setItem('historySearch', JSON.stringify(historyList.value))
+      }
+    } else {
+      showToast({
+        message: '暂无此产品，请重新搜索',
+        position: 'top',
+      });
+    }
+  })
 };
 
+// 清除搜索记录
+const clearStory = () => {
+  historyList.value = ''
+  localStorage.setItem('historySearch', JSON.stringify([]))
+}
 </script>
 
 <template>
-  <van-nav-bar
-      left-text="返回"
-      left-arrow
-      @click-left="onClickLeft">
-    <template #right>
-      <form action="/">
-        <van-search
-            class="select"
-            v-model="value"
-            show-action
-            shape="round"
-            placeholder="请输入商品关键词"
-            @search="onSearch"
-            @cancel="onCancel">
-        </van-search>
-      </form>
-    </template>
-  </van-nav-bar>
+  <div class="contain">
+    <van-nav-bar
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft">
+      <template #right>
+        <form action="/">
+          <van-search
+              style="padding: 0"
+              v-model="value"
+              show-action
+              shape="round"
+              placeholder="请输入商品关键词"
+              @search="onSearch"
+              @cancel="onCancel">
+          </van-search>
+        </form>
+      </template>
+    </van-nav-bar>
 
-  <div v-if="resp.length > 0">
-    {{ resp }}
+    <div class="search-history" v-if="historyList.length > 0">
+      <div
+          class="history-item"
+          v-for="{item, index} in historyList"
+          :key="index">
+        {{ item.title }}123
+      </div>
+      <div class="search-btn" @click="clearStory">
+        <div>清除所有搜索历史</div>
+      </div>
+    </div>
+    <div v-if="resp.length > 0">
+      {{ resp }}
+    </div>
   </div>
+
 </template>
 
 <style scoped lang="scss">
-.select{
-  padding-left: 0;
+
+.contain {
+  background-color: #efefef;
+  width: 100vw;
+  height: 100vh;
 }
 
-.van-nav-bar{
+.van-nav-bar {
   --van-nav-bar-text-color: #0c34ba;
   --van-nav-bar-icon-color: #0c34ba;
 }
+
+.search-history {
+  overflow: hidden;
+  padding-top: 12px;
+  background-color: #FFF;
+
+  .history-item{
+    background: #FFF;
+    width: 100vw;
+  }
+
+  .search-btn{
+    margin: 1rem auto 1rem auto;
+    height: 1.6rem;
+    background-color: #e8e8e8;
+    width: 10.5rem;
+    text-align: center;
+    color: #666;
+    border-radius: .8rem;
+    font-size: .9em;
+  }
+}
+
+
 </style>
